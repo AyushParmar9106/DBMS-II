5b3e305e9e6cde085276b9e4f9c7a717ab400d27
AyushParmar <143683235+AyushParmar9106@users.noreply.github.com>
2024-12-13 08:59:19 +0530

    Add files via upload

--- Lab_2.sql
+++ Lab_2.sql
@@ -146,3 +146,134 @@ UPDATE Person
 SET FirstName=@FirstName WHERE LastName = @LastName
 END
 EXEC PR_UPDATE_PERSON 'RAHUL','RAHUL'
+
+--------------------------------------------------------
+
+CREATE OR ALTER PROCEDURE PR_DEPARTMENT_SELECTBYPRIMARYKEY
+@DID INT
+AS
+BEGIN
+	SELECT * FROM Department WHERE DepartmentID=@DID
+END
+
+CREATE OR ALTER PROCEDURE PR_DESIGNATION_SELECTBYPRIMARYKEY
+@DID INT
+AS
+BEGIN
+	SELECT * FROM Designation WHERE DesignationID=@DID
+END
+
+CREATE OR ALTER PROCEDURE PR_PERSON_SELECTBYPRIMARYKEY
+@PID INT
+AS
+BEGIN
+	SELECT * FROM Person WHERE PersonID=@PID
+END
+-------------------------------------------------------------------------------------------------------------------------------------------
+CREATE OR ALTER PROCEDURE PR_PERSON_JOIN
+AS
+BEGIN
+	SELECT * FROM PERSON AS P JOIN DEPARTMENT
+	AS DEP ON P.DepartmentID=DEP.DepartmentID JOIN DESIGNATION 
+	AS desi ON p.designationID=desi.designationID
+END
+
+----------------------------------------------------------------
+CREATE OR ALTER PROCEDURE PR_PERSON_FIRST_THREE
+AS
+BEGIN
+	SELECT TOP 3 * FROM PERSON
+END
+
+--PART-B
+--5 
+CREATE OR ALTER PROCEDURE PR_DEPARTMENT_NAME
+@DeptName varchar(100)
+AS
+BEGIN
+	SELECT * FROM PERSON AS p JOIN DEPARTMENT AS d ON p.departmentID=d.departmentID WHERE d.DepartmentName=@DeptName
+END
+
+--6 
+CREATE OR ALTER PROCEDURE PR_PERSON_DISPLAY
+@DeptName varchar(100),
+@DesName varchar(100)
+AS
+BEGIN
+	SELECT FirstName,Salary,JoiningDate,DepartmentName FROM PERSON AS p join Department AS dep on p.DepartmentID=dep.DepartmentID join Designation AS desi on p.DesignationID=desi.DesignationID
+	WHERE dep.departmentName=@DeptName AND desi.designationName=@DesName
+END
+
+--7 
+CREATE OR ALTER PROCEDURE PR_PERSON_FIRSTNAME
+@FName varchar(100)
+AS
+BEGIN
+	SELECT * FROM Person AS p join department as dep on p.departmentID=dep.departmentID join designation as desi on p.designationID=desi.designationID
+	where p.FirstName=@FName
+END
+
+--8
+CREATE OR ALTER PROCEDURE PR_DEPARTMENT_MAX_MIN_SUM
+AS
+BEGIN
+	SELECT dep.DepartmentName,MAX(p.SALARY),MIN(p.SALARY),SUM(p.SALARY) FROM Department AS dep JOIN Person AS p on p.DepartmentID=dep.DepartmentID
+	group by dep.DepartmentName
+END
+
+--9
+CREATE OR ALTER PROCEDURE PR_DESIGNATION_AVG_SUM
+AS
+BEGIN
+	SELECT desi.DesignationName,AVG(p.salary),SUM(p.salary) FROM PERSON AS P JOIN DESIGNATION AS DESI ON P.DESIGNATIONID=DESI.DESIGNATIONID
+	GROUP BY desi.DesignationName
+END
+
+--PART-C
+--10
+CREATE OR ALTER PROCEDURE PR_PERSON_COUNT
+@DeptName varchar(100)
+AS
+BEGIN
+	SELECT COUNT(p.PERSONID) FROM PERSON AS p JOIN Department AS dept on p.DepartmentID=dept.DepartmentID WHERE DepartmentName=@DeptName
+END
+
+--11
+CREATE OR ALTER PROCEDURE PR_PERSON_SALARY
+@Sal Decimal(8,2)
+AS
+BEGIN
+	SELECT * FROM PERSON AS P JOIN DEPARTMENT AS DEP ON P.DEPARTMENTID=DEP.DEPARTMENTID JOIN DESIGNATION AS DESI ON P.DESIGNATIONID=DESI.DESIGNATIONID
+	WHERE P.SALARY>@Sal
+END
+
+--12
+CREATE OR ALTER PROCEDURE PR_DEPARTMENT_TOTAL_SAL
+AS
+BEGIN
+	SELECT dep.DepartmentName, SUM(p.salary) AS TotalSalary FROM Department AS dep JOIN Person AS p ON dep.DepartmentID = p.DepartmentID GROUP BY dep.DepartmentName HAVING SUM(p.salary) = (SELECT MAX(DepartmentTotal) FROM (SELECT SUM(p.salary) AS DepartmentTotal FROM Department AS d JOIN Person AS p ON d.DepartmentID = p.DepartmentID GROUP BY d.DepartmentID) AS Subquery);
+
+END
+
+--13
+CREATE OR ALTER PROCEDURE PR_DESIGNATION_LAST_TEN
+@DesigName varchar(100)
+AS
+BEGIN
+	SELECT p.FirstName,p.LastName,dep.DepartmentName FROM PERSON AS p join Department AS dep on p.DepartmentID=dep.DepartmentID join Designation AS desig on p.DesignationID=desig.DesignationID
+	where year(getDate())-year(p.JoiningDate)<=10
+END
+
+--14
+CREATE OR ALTER PROCEDURE PR_DEPARTMENT_NO_DESIGNATION
+AS
+BEGIN
+	SELECT dep.DepartmentName,p.FirstName,p.LastName FROM DEPARTMENT AS dep JOIN PERSON AS p on dep.DepartmentID=p.DepartmentID where p.DesignationID IS NULL
+END
+
+--15
+CREATE OR ALTER PROCEDURE PR_DEPARTMENT_AVG_SAL
+AS
+BEGIN
+	SELECT dept.DepartmentName,avg(p.salary) FROM DEPARTMENT AS dept JOIN PERSON AS p ON dept.DepartmentID=p.DepartmentID group by dept.DepartmentName having avg(p.salary)>12000
+END
